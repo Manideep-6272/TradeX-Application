77021241e0166a2998598897c6b416f614e89655
Mothukuri Manideep <manigoudmothukuri@gmail.com>
2026-01-13 19:03:00 +0530

    Update API endpoint for new order submissions

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -11,7 +11,7 @@ const BuyActionWindow = ({ uid }) => {
   const [stockQty,setStockQty] = useState(1);
   const [stockPrice,setStockPrice] = useState(0.0);
   const handleBuyClick = ()=>{
-    axios.post('http://localhost:3002/newOrder',{
+    axios.post('https://tradex-application.onrender.com/newOrder',{
       name: uid,
       qty: stockQty,
       price: stockPrice,
